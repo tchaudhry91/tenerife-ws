5d8536b3fde6e51152a2f7f17bcd0d6c61fcb7ab
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2019-05-30 09:53:00 +0100

    Added modules and gorilla router

--- cmd/tenerife-ws/main.go
+++ cmd/tenerife-ws/main.go
@@ -4,12 +4,16 @@ import (
 	"net"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	port := os.Getenv("PORT")
+	router := mux.NewRouter()
 	server := http.Server{
-		Addr: net.JoinHostPort("", port),
+		Addr:    net.JoinHostPort("", port),
+		Handler: router,
 	}
 	server.ListenAndServe()
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tchaudhry91/tenerife-ws
+
+go 1.12
+
+require github.com/gorilla/mux v1.7.2
